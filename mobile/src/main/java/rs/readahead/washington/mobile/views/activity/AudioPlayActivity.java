d990eb0bd0d1dc0193f6fba4f5aa0dc23e6e849b
DjordjeM <djordje.marusic@gmail.com>
2020-02-18 00:20:15 +0100

    Disable screen timeout on audio play, and enable it on pause.

--- mobile/src/main/java/rs/readahead/washington/mobile/views/activity/AudioPlayActivity.java
+++ mobile/src/main/java/rs/readahead/washington/mobile/views/activity/AudioPlayActivity.java
@@ -15,6 +15,7 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -412,6 +413,7 @@ public class AudioPlayActivity extends CacheWordSubscriberBaseActivity implement
 
         paused = false;
         disablePlay();
+        disableSleep();
     }
 
     private void handlePause() {
@@ -425,6 +427,7 @@ public class AudioPlayActivity extends CacheWordSubscriberBaseActivity implement
         if (audioPlayer != null) {
             audioPlayer.pause();
         }
+        enableSleep();
     }
 
     private void onPlayerStop() {
@@ -481,5 +484,13 @@ public class AudioPlayActivity extends CacheWordSubscriberBaseActivity implement
                         TimeUnit.MINUTES.toMinutes(TimeUnit.MILLISECONDS.toHours(duration)),
                 TimeUnit.MILLISECONDS.toSeconds(duration) -
                         TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(duration)));
+    }
+
+    private void disableSleep(){
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+    }
+
+    private void enableSleep(){
+        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
     }
 }
