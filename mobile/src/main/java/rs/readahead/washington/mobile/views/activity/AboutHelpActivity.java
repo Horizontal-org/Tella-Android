4fdee69c33563c8bd145e5d2dc9c8f2191baec1f
Djordje <djordje.marusic@gmail.com>
2020-10-06 12:21:56 +0200

    T182 - Change to About & Help section

--- mobile/src/main/java/rs/readahead/washington/mobile/views/activity/AboutHelpActivity.java
+++ mobile/src/main/java/rs/readahead/washington/mobile/views/activity/AboutHelpActivity.java
@@ -43,16 +43,15 @@ public class AboutHelpActivity extends CacheWordSubscriberBaseActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                finish();
-                return true;
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
 
-    @OnClick({R.id.setup, R.id.faq, R.id.contact, R.id.privacy, R.id.licenses, R.id.source_code})
+    @OnClick({R.id.setup, R.id.faq, R.id.contact, R.id.privacy})
     public void startActivity(View view) {
         String url = null;
 
@@ -73,14 +72,6 @@ public class AboutHelpActivity extends CacheWordSubscriberBaseActivity {
 
              case R.id.privacy:
                 url = getString(R.string.config_privacy_url);
-                break;
-
-             case R.id.licenses:
-                url = getString(R.string.config_licence_url);
-                break;
-
-             case R.id.source_code:
-                url = getString(R.string.config_source_code_url);
                 break;
         }
 
