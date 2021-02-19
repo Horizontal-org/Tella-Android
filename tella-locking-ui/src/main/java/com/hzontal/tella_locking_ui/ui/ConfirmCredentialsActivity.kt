1b4e80e05b99dae9133231f478b0c0ba8eb5d1ee
Ahlem Jarrar <ahlem.jarrar@hotmail.com>
2021-02-19 16:15:51 +0100

    Finish confirm credentials activity

---
+++ tella-locking-ui/src/main/java/com/hzontal/tella_locking_ui/ui/ConfirmCredentialsActivity.kt
@@ -0,0 +1,23 @@
+package com.hzontal.tella_locking_ui.ui
+
+import android.os.Bundle
+import android.widget.Button
+import com.hzontal.tella_locking_ui.R
+import com.hzontal.tella_locking_ui.TellaKeysUI
+import com.hzontal.tella_locking_ui.common.BaseActivity
+
+class ConfirmCredentialsActivity : BaseActivity() {
+
+    private lateinit var goToTellaButton : Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.confirm_unlocking_activity)
+        initView()
+    }
+
+    private fun initView(){
+        goToTellaButton = findViewById(R.id.finishUnlockingBtn)
+        goToTellaButton.setOnClickListener { TellaKeysUI.getCredentialsCallback().onSuccessfulUnlock(this) }
+    }
+
+}
