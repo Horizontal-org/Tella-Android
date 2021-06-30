b7dcff71e2dc0eb04527f9d9a2382d24a61ebf2c
DjordjeM <djordje.marusic@gmail.com>
2021-06-30 18:37:19 +0200

    Enter settings from home

--- mobile/src/main/java/rs/readahead/washington/mobile/views/fragment/VaultFragment.kt
+++ mobile/src/main/java/rs/readahead/washington/mobile/views/fragment/VaultFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import rs.readahead.washington.mobile.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -33,7 +34,13 @@ class VaultFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_vault, container, false)
+        val view =  inflater.inflate(R.layout.fragment_vault, container, false)
+
+        view.findViewById<View>(R.id.settings).setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_homeScreen_to_main_settings)
+        }
+        
+        return view
     }
 
     companion object {
