4bc6fec4c12a9356adcbee60e521ba63af0573ae
DjordjeM <djordje.marusic@gmail.com>
2021-07-01 00:06:14 +0200

    Servers settings fragment basic layout

--- mobile/src/main/java/rs/readahead/washington/mobile/views/settings/ServersSettings.kt
+++ mobile/src/main/java/rs/readahead/washington/mobile/views/settings/ServersSettings.kt
@@ -12,7 +12,7 @@ class ServersSettings : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_servers_settings, container, false)
-        (activity as OnFragmentSelected?)?.setToolbarLabel(R.string.settings_docu_dialog_title_server_settings)
+        (activity as OnFragmentSelected?)?.setToolbarLabel(R.string.settings_servers_title_server_settings)
 
         return view
     }
