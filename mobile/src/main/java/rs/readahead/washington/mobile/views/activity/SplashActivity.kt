4334b0f9aebb4d6cea07eff4f7a2102c727e5225
DjordjeM <djordje.marusic@gmail.com>
2021-08-23 20:21:10 +0200

    Do not show splash if app is camouflaged

--- mobile/src/main/java/rs/readahead/washington/mobile/views/activity/SplashActivity.kt
+++ mobile/src/main/java/rs/readahead/washington/mobile/views/activity/SplashActivity.kt
@@ -4,24 +4,34 @@ import android.os.Bundle
 import android.os.Handler
 import rs.readahead.washington.mobile.MyApplication
 import rs.readahead.washington.mobile.R
+import rs.readahead.washington.mobile.util.CamouflageManager
 import rs.readahead.washington.mobile.views.base_ui.BaseActivity
 
 private const val SPLASH_TIMEOUT_MS = 1000L
 class SplashActivity : BaseActivity() {
+
+    private val cm = CamouflageManager.getInstance()
+
     private lateinit var handler : Handler
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.splash_layout)
+        if (cm.isDefaultLauncherActivityAlias) setContentView(R.layout.splash_layout)
         handler = Handler()
         initView()
     }
 
     private fun initView() {
-        handler.postDelayed({
-            MyApplication.startMainActivity(this@SplashActivity)
-            finish()
-        }, SPLASH_TIMEOUT_MS)
+        if (cm.isDefaultLauncherActivityAlias) {
+            handler.postDelayed({
+                goToMainActivity()
+            }, SPLASH_TIMEOUT_MS)
+        } else {
+            goToMainActivity()
+        }
     }
-
-
+    
+    private fun goToMainActivity(){
+        MyApplication.startMainActivity(this@SplashActivity)
+        finish()
+    }
 }
