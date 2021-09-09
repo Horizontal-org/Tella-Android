bf73f6a7233c2c7552ee6d607e004a9a7b212b9e
DjordjeM <djordje.marusic@google.com>
2021-09-09 21:22:00 +0200

    Fragment mic init

---
+++ mobile/src/main/java/rs/readahead/washington/mobile/views/base_ui/MetadataBaseLockFragment.kt
@@ -0,0 +1,44 @@
+package rs.readahead.washington.mobile.views.base_ui
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import rs.readahead.washington.mobile.views.activity.MetadataActivity
+import timber.log.Timber
+
+open class MetadataBaseLockFragment  : Fragment(){
+
+    protected lateinit var activity: MetadataActivity
+
+
+    override fun onAttach(context: Context) {
+        Timber.d("***** ${this.javaClass.name} onAttach")
+
+        super.onAttach(context)
+
+        activity = context as MetadataActivity
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        Timber.d("***** ${this.javaClass.name} onCreateView")
+
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        Timber.d("***** ${this.javaClass.name} onViewCreated")
+
+        super.onViewCreated(view, savedInstanceState)
+    }
+    override fun onResume() {
+        activity.restrictActivity()
+        super.onResume()
+    }
+}
--- mobile/src/main/java/rs/readahead/washington/mobile/views/fragment/MicFragment.kt
+++ mobile/src/main/java/rs/readahead/washington/mobile/views/fragment/MicFragment.kt
@@ -1,17 +1,78 @@
 package rs.readahead.washington.mobile.views.fragment
 
+import android.Manifest
+import android.animation.AnimatorInflater
+import android.animation.ObjectAnimator
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import com.hzontal.tella_vault.VaultFile
+import com.hzontal.tella_vault.filter.FilterType
+import io.reactivex.disposables.CompositeDisposable
+import permissions.dispatcher.NeedsPermission
+import permissions.dispatcher.OnNeverAskAgain
+import permissions.dispatcher.OnPermissionDenied
 import rs.readahead.washington.mobile.R
+import rs.readahead.washington.mobile.media.AudioRecorder
+import rs.readahead.washington.mobile.media.AudioRecorder.AudioRecordInterface
+import rs.readahead.washington.mobile.media.MediaFileHandler
+import rs.readahead.washington.mobile.mvp.contract.IAudioCapturePresenterContract
+import rs.readahead.washington.mobile.mvp.contract.IMetadataAttachPresenterContract
+import rs.readahead.washington.mobile.mvp.contract.ITellaFileUploadSchedulePresenterContract
+import rs.readahead.washington.mobile.mvp.presenter.AudioCapturePresenter
+import rs.readahead.washington.mobile.mvp.presenter.MetadataAttacher
+import rs.readahead.washington.mobile.mvp.presenter.TellaFileUploadSchedulePresenter
+import rs.readahead.washington.mobile.util.StringUtils
+import rs.readahead.washington.mobile.views.activity.GalleryActivity
+import rs.readahead.washington.mobile.views.base_ui.MetadataBaseLockFragment
+import timber.log.Timber
+
+import java.util.*
+import java.util.concurrent.TimeUnit
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-class MicFragment : Fragment() {
+class MicFragment : MetadataBaseLockFragment(), AudioRecordInterface,
+    IAudioCapturePresenterContract.IView,
+    ITellaFileUploadSchedulePresenterContract.IView,
+    IMetadataAttachPresenterContract.IView {
+
     // TODO: Rename and change types of parameters
+    private val TIME_FORMAT: String = "%02d:%02d:%02d"
+    var RECORDER_MODE = "rm"
+
+    private var animator: ObjectAnimator? = null
+
+    private var notRecording = false
+
+    private val UPDATE_SPACE_TIME_MS: Long = 60000
+    private var lastUpdateTime: Long = 0
+
+    // handling MediaFile
+    private var handlingMediaFile: VaultFile? = null
+
+    // recording
+    private var audioRecorder: AudioRecorder? = null
+    private var uploadPresenter: TellaFileUploadSchedulePresenter? = null
+    private var presenter: AudioCapturePresenter? = null
+    private var metadataAttacher: MetadataAttacher? = null
+    private val disposable = CompositeDisposable()
+    private val rationaleDialog: AlertDialog? = null
+
+    lateinit var mRecord: ImageButton
+    lateinit var mPlay: ImageButton
+    lateinit var mStop: ImageButton
+    lateinit var mTimer: TextView
+    lateinit var freeSpace: TextView
+    lateinit var redDot: ImageView
+
     private var param1: String? = null
     private var param2: String? = null
 
@@ -25,8 +86,44 @@ class MicFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mic, container, false)
+        val view = inflater.inflate(R.layout.fragment_mic, container, false)
+        initView(view)
+        return view
+    }
+
+    fun initView(view: View) {
+        mRecord = view.findViewById(R.id.record_audio)
+        mPlay = view.findViewById(R.id.play_audio)
+        mStop = view.findViewById(R.id.stop_audio)
+        mTimer = view.findViewById(R.id.audio_time)
+        freeSpace = view.findViewById(R.id.free_space)
+        redDot = view.findViewById(R.id.red_dot)
+
+        mRecord.setOnClickListener {
+            if (notRecording) {
+                handleRecord()
+            } else {
+                handlePause()
+            }
+        }
+
+        mStop.setOnClickListener { handleStop() }
+
+        mPlay.setOnClickListener { openRecordings() }
+
+        presenter = AudioCapturePresenter(this)
+        uploadPresenter = TellaFileUploadSchedulePresenter(this)
+        metadataAttacher = MetadataAttacher(this)
+
+        notRecording = true
+
+        animator = AnimatorInflater.loadAnimator(
+            activity,
+            R.animator.fade_in
+        ) as ObjectAnimator
+
+        mTimer.text = timeToString(0)
+        disableStop()
     }
 
     companion object {
@@ -47,5 +144,303 @@ class MicFragment : Fragment() {
                         putString(ARG_PARAM2, param2)
                     }
                 }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        activity.startLocationMetadataListening()
+    }
+
+    override fun onStop() {
+        activity.stopLocationMetadataListening()
+        if (rationaleDialog != null && rationaleDialog.isShowing) {
+            rationaleDialog.dismiss()
+        }
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        animator!!.end()
+        animator = null
+        disposable.dispose()
+        cancelRecorder()
+        stopPresenter()
+        super.onDestroy()
+    }
+
+    /*override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String?>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        AudioRecordActivity2PermissionsDispatcher.onRequestPermissionsResult(
+            this,
+            requestCode,
+            grantResults
+        )
+    }*/
+
+    @OnPermissionDenied(Manifest.permission.RECORD_AUDIO)
+    fun onRecordAudioPermissionDenied() {
+    }
+
+    @OnNeverAskAgain(Manifest.permission.RECORD_AUDIO)
+    fun onRecordAudioNeverAskAgain() {
+    }
+
+    @NeedsPermission(Manifest.permission.RECORD_AUDIO)
+    fun handleRecord() {
+        notRecording = false
+        if (audioRecorder == null) {   //first start or restart
+            disablePlay()
+            handlingMediaFile = null
+            cancelRecorder()
+            audioRecorder = AudioRecorder(this)
+            disposable.add(
+                audioRecorder!!.startRecording()
+                    .subscribe(
+                        { vaultFile: VaultFile? ->
+                            onRecordingStopped(
+                                vaultFile
+                            )
+                        }
+                    ) { throwable: Throwable? ->
+                        Timber.d(throwable)
+                        onRecordingError()
+                    }
+            )
+        } else {
+            cancelPauseRecorder()
+        }
+        disableRecord()
+        enableStop()
+    }
+
+    override fun onDurationUpdate(duration: Long) {
+        activity.runOnUiThread(Runnable { mTimer.setText(timeToString(duration)) })
+
+        if (duration > UPDATE_SPACE_TIME_MS + lastUpdateTime) {
+            lastUpdateTime += UPDATE_SPACE_TIME_MS
+            presenter?.checkAvailableStorage()
+        }
+    }
+
+    override fun onAddingStart() {
+    }
+
+    override fun onAddingEnd() {
+    }
+
+    override fun onAddSuccess(vaultFile: VaultFile) {
+        activity.attachMediaFileMetadata(vaultFile, metadataAttacher)
+        activity.showToast(
+            String.format(
+                getString(R.string.recorder_toast_recording_saved),
+                getString(R.string.app_name)
+            )
+        )
+    }
+
+    override fun onAddError(error: Throwable?) {
+        activity.showToast(R.string.gallery_toast_fail_saving_file)
+    }
+
+    override fun onAvailableStorage(memory: Long) {
+        updateStorageSpaceLeft(memory)
+    }
+
+    override fun onAvailableStorageFailed(throwable: Throwable?) {
+    }
+
+    override fun onMetadataAttached(vaultFile: VaultFile?) {
+        /*val intent = Intent()
+
+        if (mode == AudioRecordActivity2.Mode.COLLECT) {
+            intent.putExtra(QuestionAttachmentActivity.MEDIA_FILE_KEY, handlingMediaFile)
+        } else {
+            intent.putExtra(C.CAPTURED_MEDIA_FILE_ID, vaultFile!!.id)
+        }*/
+
+        //setResult(Activity.RESULT_OK, intent)
+        mTimer.text = timeToString(0)
+
+        //scheduleFileUpload(handlingMediaFile)
+    }
+
+    override fun onMetadataAttachError(throwable: Throwable?) {
+        activity.showToast(R.string.gallery_toast_fail_saving_file)
+    }
+
+    override fun onMediaFilesUploadScheduled() {
+    }
+
+    override fun onMediaFilesUploadScheduleError(throwable: Throwable?) {
+    }
+
+
+    override fun onGetMediaFilesSuccess(mediaFiles: MutableList<VaultFile>?) {
+    }
+
+    override fun onGetMediaFilesError(error: Throwable?) {
+    }
+
+    //    private void returnData() {
+    //        if (handlingMediaFile != null) {
+    //            presenter.addMediaFile(handlingMediaFile);
+    //        }
+    //    }
+    private fun openRecordings() {
+        val intent: Intent = Intent(activity, GalleryActivity::class.java)
+        intent.putExtra(GalleryActivity.GALLERY_FILTER, FilterType.AUDIO.name)
+        intent.putExtra(GalleryActivity.GALLERY_ALLOWS_ADDING, false)
+        startActivity(intent)
+    }
+
+    private fun handleStop() {
+        notRecording = true
+        stopRecorder()
+    }
+
+    private fun handlePause() {
+        pauseRecorder()
+        enableRecord()
+        notRecording = true
+    }
+
+    private fun onRecordingStopped(vaultFile: VaultFile?) {
+        if (vaultFile == null) {
+            handlingMediaFile = null
+            disableStop()
+            disablePlay()
+            enableRecord()
+        } else {
+            handlingMediaFile = vaultFile
+            handlingMediaFile!!.size = MediaFileHandler.getSize(vaultFile)
+            disableStop()
+            enablePlay()
+            enableRecord()
+
+            // returnData();
+            onAddSuccess(vaultFile)
+        }
+    }
+
+    private fun onRecordingError() {
+        handlingMediaFile = null
+        disableStop()
+        disablePlay()
+        enableRecord()
+        mTimer.text = timeToString(0)
+        activity.showToast(R.string.recorder_toast_fail_recording)
+    }
+
+    private fun disableRecord() {
+        mRecord.background = activity.resources.getDrawable(R.drawable.white_circle_background)
+        mRecord.setImageResource(R.drawable.ic_pause_black_24dp)
+        redDot.visibility = View.VISIBLE
+        animator!!.target = redDot
+        animator!!.start()
+    }
+
+    private fun enableRecord() {
+        mRecord.background =
+            activity.resources.getDrawable(R.drawable.audio_record_button_background)
+        mRecord.setImageResource(R.drawable.ic_mic_white)
+        redDot.visibility = View.GONE
+        animator!!.end()
+    }
+
+    private fun disablePlay() {
+        disableButton(mPlay)
+    }
+
+    private fun enablePlay() {
+        enableButton(mPlay)
+    }
+
+    private fun disableStop() {
+        disableButton(mStop)
+    }
+
+    private fun enableStop() {
+        enableButton(mStop)
+    }
+
+    private fun enableButton(button: ImageButton) {
+        button.isEnabled = true
+        button.alpha = 1f
+    }
+
+    private fun disableButton(button: ImageButton) {
+        button.isEnabled = false
+        button.alpha = .2f
+    }
+
+   /* private fun openRecordings() {
+        val intent: Intent = Intent(this, GalleryActivity::class.java)
+        intent.putExtra(GalleryActivity.GALLERY_FILTER, FilterType.AUDIO.name)
+        intent.putExtra(GalleryActivity.GALLERY_ALLOWS_ADDING, false)
+        startActivity(intent)
+    }*/
+
+    private fun stopRecorder() {
+        if (audioRecorder != null) {
+            audioRecorder!!.stopRecording()
+            audioRecorder = null
+        }
+    }
+
+    private fun pauseRecorder() {
+        if (audioRecorder != null) {
+            audioRecorder!!.pauseRecording()
+        }
+    }
+
+    private fun cancelPauseRecorder() {
+        if (audioRecorder != null) {
+            audioRecorder!!.cancelPause()
+        }
+    }
+
+    private fun cancelRecorder() {
+        if (audioRecorder != null) {
+            audioRecorder!!.cancelRecording()
+            audioRecorder = null
+        }
+    }
+
+    private fun stopPresenter() {
+        if (presenter != null) {
+            presenter!!.destroy()
+            presenter = null
+        }
+    }
+
+    private fun timeToString(duration: Long): String {
+        return String.format(
+            Locale.ROOT, TIME_FORMAT,
+            TimeUnit.MILLISECONDS.toHours(duration),
+            TimeUnit.MILLISECONDS.toMinutes(duration) -
+                    TimeUnit.MINUTES.toMinutes(TimeUnit.MILLISECONDS.toHours(duration)),
+            TimeUnit.MILLISECONDS.toSeconds(duration) -
+                    TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(duration))
+        )
+    }
+
+    private fun updateStorageSpaceLeft(memoryLeft: Long) {
+        val timeMinutes = memoryLeft / 262144.0 // 4 minutes --> 1MB approximation/1024*256
+        // todo: move this (262144.0) number to recorder to provide
+        val days = (timeMinutes / 1440).toInt()
+        val hours = ((timeMinutes - days * 1440) / 60).toInt()
+        val minutes = (timeMinutes - days * 1440 - hours * 60).toInt()
+        val spaceLeft = StringUtils.getFileSize(memoryLeft)
+        if (days < 1 && hours < 12) {
+            freeSpace.text =
+                getString(R.string.recorder_meta_space_available_hours, hours, minutes, spaceLeft)
+        } else {
+            freeSpace.text =
+                getString(R.string.recorder_meta_space_available_days, days, hours, spaceLeft)
+        }
     }
 }
