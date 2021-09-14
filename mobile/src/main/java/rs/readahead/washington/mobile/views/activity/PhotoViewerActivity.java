678d66db22931caf3f0209d018828ccec5bcfdd3
DjordjeM <djordje.marusic@google.com>
2021-09-14 20:16:52 +0200

    remove metadata fragment

--- mobile/src/main/java/rs/readahead/washington/mobile/views/activity/PhotoViewerActivity.java
+++ mobile/src/main/java/rs/readahead/washington/mobile/views/activity/PhotoViewerActivity.java
@@ -46,7 +46,6 @@ import rs.readahead.washington.mobile.presentation.entity.VaultFileLoaderModel;
 import rs.readahead.washington.mobile.util.DialogsUtil;
 import rs.readahead.washington.mobile.util.PermissionUtil;
 import rs.readahead.washington.mobile.views.base_ui.BaseLockActivity;
-import rs.readahead.washington.mobile.views.fragment.MetadataViewerFragment;
 import rs.readahead.washington.mobile.views.fragment.ShareDialogFragment;
 import rs.readahead.washington.mobile.views.fragment.vault.info.VaultInfoFragment;
 
@@ -324,11 +323,6 @@ public class PhotoViewerActivity extends BaseLockActivity implements
         Intent viewMetadata = new Intent(this, MetadataViewerActivity.class);
         viewMetadata.putExtra(VIEW_METADATA, vaultFile);
         startActivity(viewMetadata);
-       /* toolbar.setStartTextTitle(getString(R.string.vault_file_info));
-        menu.findItem(R.id.menu_item_more).setVisible(false);
-        menu.findItem(R.id.menu_item_metadata).setVisible(false);
-        invalidateOptionsMenu();
-        addFragment(new MetadataViewerFragment().newInstance(vaultFile),R.id.photo_viewer_container);*/
     }
 
     private void stopPresenter() {
