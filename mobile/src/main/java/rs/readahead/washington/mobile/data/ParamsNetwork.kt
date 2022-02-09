145cda13177602a0f2fcc194e2f3320743bd714a
Ahlem Jarrar <ahlem.jarrar@hotmail.com>
2022-02-09 14:58:33 +0100

    remove api constant from url

--- mobile/src/main/java/rs/readahead/washington/mobile/data/ParamsNetwork.kt
+++ mobile/src/main/java/rs/readahead/washington/mobile/data/ParamsNetwork.kt
@@ -1,10 +1,10 @@
 package rs.readahead.washington.mobile.data
 
 object ParamsNetwork {
-    const val URL_LOGIN = "login"
-    const val URL_TEMPLATES = "templates"
-    const val URL_ENTITIES = "entities"
-    const val URL_SETTINGS = "settings"
-    const val URL_TRANSLATE_SETTINGS = "translations/setasdeafult"
-    const val UWAZI_BASE_URL = "https://horizontal.uwazi.io/api/"
+    private const val URL_API = "/api/"
+    const val URL_LOGIN = "${URL_API}login"
+    const val URL_TEMPLATES = "${URL_API}templates"
+    const val URL_ENTITIES = "${URL_API}entities"
+    const val URL_SETTINGS = "${URL_API}settings"
+    const val URL_TRANSLATE_SETTINGS = "${URL_API}translations/setasdeafult"
 }
