23a546f66e1722b6c9bf1e55bc31729a9a63aa4a
DjordjeM <djordje.marusic@google.com>
2022-04-04 20:57:09 +0200

    Check if label of url is empty

--- mobile/src/main/java/rs/readahead/washington/mobile/views/fragment/uwazi/widgets/UwaziLinkWidget.java
+++ mobile/src/main/java/rs/readahead/washington/mobile/views/fragment/uwazi/widgets/UwaziLinkWidget.java
@@ -108,8 +108,13 @@ public class UwaziLinkWidget extends UwaziQuestionWidget {
         clearFocus();
         String l = label.getText().toString();
         String u = url.getText().toString();
-        if ((!l.isEmpty() && !URLUtil.isValidUrl(u)) || (l.isEmpty() && !u.isEmpty())) {
+
+        if ((!l.isEmpty() && !(URLUtil.isValidUrl(u)))) {
             this.setConstraintValidationText(getContext().getString(R.string.Uwazi_Info_NotValidURL));
+        }
+
+        if (l.isEmpty() && !(isEmptyUrl(u))) {
+            this.setConstraintValidationText(getContext().getString(R.string.Uwazi_Info_UrlLabelEmpty));
         }
 
         if (TextUtils.isEmpty(l) && TextUtils.isEmpty(u)) {
@@ -118,6 +123,10 @@ public class UwaziLinkWidget extends UwaziQuestionWidget {
             UwaziLink link = new UwaziLink(l, u);
             return new UwaziValue(link);
         }
+    }
+
+    private boolean isEmptyUrl(String url) {
+        return url.equals(getContext().getString(R.string.Uwazi_Answer_LinkInitialText)) || url.isEmpty();
     }
 
     @Override
