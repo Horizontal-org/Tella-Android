bf9a9c6a94ed1a55c65c4e6bd6813e497d40b192
djordjemarusic <djordje.marusic@gmail.com>
2022-10-19 10:41:08 +0200

    Turn off KeyboardUtil on BottomSheets on Androids 9+

--- shared-ui/src/main/java/org/hzontal/shared_ui/bottomsheet/CustomBottomSheetFragment.kt
+++ shared-ui/src/main/java/org/hzontal/shared_ui/bottomsheet/CustomBottomSheetFragment.kt
@@ -219,7 +219,9 @@ class CustomBottomSheetFragment : BottomSheetDialogFragment() {
         configBackPressCallback()
         if (statusBarColor != null) applyStatusBarColor(statusBarColor!!)
 
-        KeyboardUtil(activity, view)
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.O_MR1) {
+            KeyboardUtil(activity, view)
+        }
     }
 
     private fun configBackPressCallback() {
