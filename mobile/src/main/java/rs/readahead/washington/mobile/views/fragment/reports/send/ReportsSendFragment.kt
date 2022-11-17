9c6cf28d37e593558f7f893c0573b4e5180ec0dd
DjordjeM <djordje.marusic@google.com>
2022-11-17 22:38:06 +0100

    ReportsSendFragment wip

--- mobile/src/main/java/rs/readahead/washington/mobile/views/fragment/reports/send/ReportsSendFragment.kt
+++ mobile/src/main/java/rs/readahead/washington/mobile/views/fragment/reports/send/ReportsSendFragment.kt
@@ -2,14 +2,11 @@ package rs.readahead.washington.mobile.views.fragment.reports.send
 
 import android.os.Bundle
 import android.view.View
-import com.google.gson.Gson
 import dagger.hilt.android.AndroidEntryPoint
 import rs.readahead.washington.mobile.databinding.FragmentSendReportBinding
 import rs.readahead.washington.mobile.domain.entity.reports.ReportFormInstance
-import rs.readahead.washington.mobile.domain.entity.uwazi.UwaziEntityInstance
 import rs.readahead.washington.mobile.views.base_ui.BaseBindingFragment
 import rs.readahead.washington.mobile.views.fragment.reports.entry.BUNDLE_REPORT_FORM_INSTANCE
-import rs.readahead.washington.mobile.views.fragment.uwazi.send.SEND_ENTITY
 import rs.readahead.washington.mobile.views.fragment.uwazi.widgets.ReportsFormEndView
 
 @AndroidEntryPoint
@@ -17,7 +14,7 @@ class ReportsSendFragment :
     BaseBindingFragment<FragmentSendReportBinding>(FragmentSendReportBinding::inflate) {
 
     private lateinit var endView: ReportsFormEndView
-    private var entityInstance: ReportFormInstance? = null
+    private var reportInstance: ReportFormInstance? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,18 +31,28 @@ class ReportsSendFragment :
 
     private fun initView() {
         arguments?.let {
-            entityInstance =  it.get(BUNDLE_REPORT_FORM_INSTANCE) as ReportFormInstance
-
+            reportInstance = it.get(BUNDLE_REPORT_FORM_INSTANCE) as ReportFormInstance
             showFormEndView()
+        }
+
+        with(binding) {
+            this?.toolbar?.backClickListener = { nav().popBackStack() }
+
+            this?.nextBtn?.setOnClickListener {
+                reportInstance?.let {
+                    submitEntity()
+                }
+            }
+
         }
     }
 
     private fun showFormEndView() {
-        if (entityInstance == null) {
+        if (reportInstance == null) {
             return
         }
 
-        entityInstance?.let { reportFormInstance ->
+        reportInstance?.let { reportFormInstance ->
             endView = ReportsFormEndView(
                 activity,
                 reportFormInstance.title,
@@ -56,4 +63,12 @@ class ReportsSendFragment :
             binding?.endViewContainer?.addView(endView)
         }
     }
+
+    private fun submitEntity() {
+        reportInstance?.let { entity ->
+
+        }
+    }
+
+
 }
