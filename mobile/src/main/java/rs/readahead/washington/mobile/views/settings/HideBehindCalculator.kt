06a31f28688dc4ea3513226b96e560a6053b3475
wafa <lakwafa@gmail.com>
2023-02-23 16:55:06 +0100

    adding next and back button  in sliding between calculator skins

--- mobile/src/main/java/rs/readahead/washington/mobile/views/settings/HideBehindCalculator.kt
+++ mobile/src/main/java/rs/readahead/washington/mobile/views/settings/HideBehindCalculator.kt
@@ -8,33 +8,41 @@ import androidx.core.os.bundleOf
 import rs.readahead.washington.mobile.R
 import rs.readahead.washington.mobile.databinding.OnboardCalculatorFragmentBinding
 import rs.readahead.washington.mobile.views.base_ui.BaseFragment
+import kotlin.properties.Delegates
 
 class HideBehindCalculator : BaseFragment() {
 
-    private lateinit var binding : OnboardCalculatorFragmentBinding
+    private lateinit var binding: OnboardCalculatorFragmentBinding
+
     companion object {
-        private const val ARG_POSITION = "ARG_POSITION"
+        private const val VIEWPAGER_POSITION = "VIEWPAGER_POSITION"
         fun getInstance(position: Int) = HideBehindCalculator().apply {
-            arguments = bundleOf(ARG_POSITION to position)
+            arguments = bundleOf(VIEWPAGER_POSITION to position)
         }
     }
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
-        binding = OnboardCalculatorFragmentBinding.inflate(layoutInflater,container,false)
+        binding = OnboardCalculatorFragmentBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val position = requireArguments().getInt(ARG_POSITION)
+        initView(view)
+    }
+
+    override fun initView(view: View) {
+
+        val position = requireArguments().getInt(VIEWPAGER_POSITION)
         var drawables = resources.obtainTypedArray(R.array.calculator_skin_array)
         with(binding) {
             this.calculatorImg.setImageDrawable(drawables.getDrawable(position))
 
         }
-    }
-    override fun initView(view: View) {
+
     }
 
 
