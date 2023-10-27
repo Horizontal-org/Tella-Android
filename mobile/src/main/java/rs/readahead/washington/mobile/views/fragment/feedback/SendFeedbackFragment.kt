e60ef8ab6e3bbe6835a7e464f7357036c323279b
wafa <lakwafa@gmail.com>
2023-10-27 18:01:44 +0100

    work on edittext display in feedback screen

--- mobile/src/main/java/rs/readahead/washington/mobile/views/fragment/feedback/SendFeedbackFragment.kt
+++ mobile/src/main/java/rs/readahead/washington/mobile/views/fragment/feedback/SendFeedbackFragment.kt
@@ -1,27 +1,29 @@
 package rs.readahead.washington.mobile.views.fragment.feedback
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.CompoundButton
 import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
+import org.hzontal.shared_ui.bottomsheet.BottomSheetUtils
+import org.hzontal.shared_ui.utils.DialogUtils
+import rs.readahead.washington.mobile.R
 import rs.readahead.washington.mobile.data.entity.feedback.FeedbackBodyEntity
 import rs.readahead.washington.mobile.databinding.FragmentSendFeedbackBinding
 
 @AndroidEntryPoint
 class SendFeedbackFragment : Fragment() {
     private var binding: FragmentSendFeedbackBinding? = null
-    private  var feedbackInstance : FeedbackBodyEntity? = null
+    private var feedbackInstance: FeedbackBodyEntity? = null
 
     companion object {
         fun newInstance() = SendFeedbackFragment()
     }
 
-   // private lateinit var viewModel: SendFeedbackViewModel
     private val viewModel by viewModels<SendFeedbackViewModel>()
 
     override fun onCreateView(
@@ -40,12 +42,51 @@ class SendFeedbackFragment : Fragment() {
             binding?.newFeedbackEditDescription?.isVisible = isChecked
 
         }
-        feedbackInstance  = FeedbackBodyEntity("ANDROID","test test")
+        feedbackInstance = FeedbackBodyEntity("ANDROID", binding?.newFeedbackEditDescription.toString())
 
         binding?.sendFeedbackBtn?.setOnClickListener {
             feedbackInstance?.let { it1 ->
-                viewModel.submitFeedback(it1,true) }
+                viewModel.submitFeedback(it1, true)
+            }
+
         }
 
-}
+//        BottomSheetUtils.showConfirmSheet(
+//                fragmentManager = parentFragmentManager,
+//                getString(R.string.save_draft),
+//                getString(R.string.verification_prompt_dialog_expl),
+//                getString(R.string.verification_prompt_action_enable_GPS),
+//                getString(R.string.verification_prompt_action_ignore),
+//                object : BottomSheetUtils.ActionConfirmed {
+//                    override fun accept(isConfirmed: Boolean) {
+//                        if (isConfirmed) {
+//
+//                        } else {
+//                        }
+//                    }
+//                })
+
+        initObservers()
+
+
+    }
+
+    private fun initObservers() {
+        viewModel.feedbackSubmitted.observe(viewLifecycleOwner) {
+            onFeedbackSubmittedSuccess()
+            activity?.onBackPressed()
+        }
+        with(viewModel) {
+            progress.observe(
+                    viewLifecycleOwner,
+            ) {
+                binding?.progressCircular?.isVisible = it
+            }
+        }
+    }
+
+    private fun onFeedbackSubmittedSuccess() {
+        DialogUtils.showBottomMessage(activity, getString(R.string.thanks_for_your_feedback), true)
+    }
+
 }
--- mobile/src/main/java/rs/readahead/washington/mobile/views/fragment/feedback/SendFeedbackViewModel.kt
+++ mobile/src/main/java/rs/readahead/washington/mobile/views/fragment/feedback/SendFeedbackViewModel.kt
@@ -1,12 +1,15 @@
 package rs.readahead.washington.mobile.views.fragment.feedback
 
 import android.annotation.SuppressLint
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.google.firebase.crashlytics.FirebaseCrashlytics
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
+import rs.readahead.washington.mobile.bus.SingleLiveEvent
 import rs.readahead.washington.mobile.data.database.DataSource
 import rs.readahead.washington.mobile.data.entity.feedback.FeedbackBodyEntity
 import rs.readahead.washington.mobile.domain.repository.feedback.FeedbackRepository
@@ -16,30 +19,46 @@ import javax.inject.Inject
 
 @HiltViewModel
 class SendFeedbackViewModel @Inject constructor(
-    private val feedbackRepository: FeedbackRepository,
-    private val dataSource: DataSource
+        private val feedbackRepository: FeedbackRepository,
+        private val dataSource: DataSource
 ) : ViewModel() {
 
     private val disposables = CompositeDisposable()
+
+    // LiveData to communicate with the view
+    private val _feedbackSubmitted = SingleLiveEvent<Boolean>()
+    val feedbackSubmitted: LiveData<Boolean>
+        get() = _feedbackSubmitted
+
+    private val _progress = MutableLiveData<Boolean>()
+    val progress: LiveData<Boolean> get() = _progress
 
     @SuppressLint("CheckResult")
     fun submitFeedback(instance: FeedbackBodyEntity, backButtonPressed: Boolean) {
 
         disposables.add(
-            feedbackRepository.submitFeedback(instance)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .doFinally { }
-                .subscribe({
-                    it.apply {
-                        var test = it.createdAt
-                    }
+                feedbackRepository.submitFeedback(instance)
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .doOnSubscribe { _progress.postValue(true) }
+                        .doFinally { _progress.postValue(false) }
+                        .subscribe({
+                            it.apply {
+                                var test = it.createdAt
+                                _feedbackSubmitted.postValue(true)
+                            }
 
-                }) { throwable: Throwable? ->
-                    Timber.d(throwable)
-                    FirebaseCrashlytics.getInstance().recordException(throwable!!)
-                })
+                        }) { throwable: Throwable? ->
+                            Timber.d(throwable)
+                            FirebaseCrashlytics.getInstance().recordException(throwable!!)
+                        })
 
     }
+
+    override fun onCleared() {
+        super.onCleared()
+        disposables.clear()
+    }
+
 
 }
