645d1e15e1ea91b40d7fd797140d39d594eabd6f
wafa <lakwafa@gmail.com>
2023-12-11 14:06:52 +0100

    fix generalSettings click regression

--- mobile/src/main/java/rs/readahead/washington/mobile/views/settings/MainSettings.kt
+++ mobile/src/main/java/rs/readahead/washington/mobile/views/settings/MainSettings.kt
@@ -1,13 +1,15 @@
 package rs.readahead.washington.mobile.views.settings
 
 import android.os.Bundle
-import android.view.*
+import android.view.View
 import rs.readahead.washington.mobile.R
-import rs.readahead.washington.mobile.R.*
+import rs.readahead.washington.mobile.R.string
 import rs.readahead.washington.mobile.databinding.FragmentMainSettingsBinding
 import rs.readahead.washington.mobile.views.base_ui.BaseBindingFragment
 
-class MainSettings : BaseBindingFragment<FragmentMainSettingsBinding>(FragmentMainSettingsBinding::inflate), View.OnClickListener {
+class MainSettings :
+    BaseBindingFragment<FragmentMainSettingsBinding>(FragmentMainSettingsBinding::inflate),
+    View.OnClickListener {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -29,19 +31,19 @@ class MainSettings : BaseBindingFragment<FragmentMainSettingsBinding>(FragmentMa
 
     override fun onClick(v: View?) {
         when (v?.id) {
-            R.id.general_settings -> {
+            R.id.general_settings_button -> {
                 nav().navigate(R.id.action_main_to_general_settings)
             }
 
-            R.id.security_settings -> {
+            R.id.security_settings_button -> {
                 nav().navigate(R.id.action_main_settings_to_security_settings)
             }
 
-            R.id.servers_settings -> {
+            R.id.servers_settings_button -> {
                 nav().navigate(R.id.action_main_settings_to_servers_settings)
             }
 
-            R.id.about_n_help_settings -> {
+            R.id.about_n_help_settings_button -> {
                 nav().navigate(R.id.action_main_settings_to_about_n_help_settings)
             }
 
