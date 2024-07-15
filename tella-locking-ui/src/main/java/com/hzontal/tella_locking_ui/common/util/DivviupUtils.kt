c5438f103568b6d54a10a56f4f524b50f76b55cf
DjordjeM <djordje.marusic@google.com>
2024-07-15 15:23:50 +0200

    Analytics install event params

--- tella-locking-ui/src/main/java/com/hzontal/tella_locking_ui/common/util/DivviupUtils.kt
+++ tella-locking-ui/src/main/java/com/hzontal/tella_locking_ui/common/util/DivviupUtils.kt
@@ -17,9 +17,9 @@ object DivviupUtils {
             try {
                 val taskId = TaskId.parse(context.getString(R.string.divviup_count_unlocks_id))
                 val leaderEndpoint: URI =
-                    URI.create(context.getString(R.string.divviup_count_unlocks_leader))
+                    URI.create(context.getString(R.string.divviup_leader))
                 val helperEndpoint: URI =
-                    URI.create(context.getString(R.string.divviup_count_unlocks_helper))
+                    URI.create(context.getString(R.string.divviup_helper))
                 val timePrecisionSeconds: Long =
                     context.resources.getInteger(R.integer.divviup_count_unlocks_timePrecisionSeconds)
                         .toLong()
@@ -43,9 +43,18 @@ object DivviupUtils {
         if (!CommonPreferences.hasAcceptedAnalytics() || CommonPreferences.isInstallMetricSent()) return
         Executors.newSingleThreadExecutor().execute {
             try {
-                /*
-                * parameterize the call
-                */
+                val taskId = TaskId.parse(context.getString(R.string.divviup_count_installs_id))
+                val leaderEndpoint: URI =
+                    URI.create(context.getString(R.string.divviup_leader))
+                val helperEndpoint: URI =
+                    URI.create(context.getString(R.string.divviup_helper))
+                val timePrecisionSeconds: Long =
+                    context.resources.getInteger(R.integer.divviup_count_unlocks_timePrecisionSeconds)
+                        .toLong()
+                val client = Client.createPrio3Count(
+                    context, leaderEndpoint, helperEndpoint, taskId, timePrecisionSeconds
+                )
+                client.sendMeasurement(true)
                 CommonPreferences.setInstallMetricSent(true)
                 Timber.d("Divviup runInstallEvent measurement sent")
             } catch (e: Exception) {
