7c08b91341ca83e8a2c8713d19cc7173ee3d9365
Ahlem Jarrar <ahlem.jarrar@hotmail.com>
2024-08-22 11:02:41 +0100

    T579 - Add tutorial screen for data collection

--- mobile/src/main/java/rs/readahead/washington/mobile/views/activity/onboarding/OnboardCollectDataFragment.kt
+++ mobile/src/main/java/rs/readahead/washington/mobile/views/activity/onboarding/OnboardCollectDataFragment.kt
@@ -8,40 +8,36 @@ import android.widget.TextView
 import rs.readahead.washington.mobile.R
 import rs.readahead.washington.mobile.views.base_ui.BaseFragment
 
-class OnBoardRecorderFragment : BaseFragment() {
+class OnboardCollectDataFragment : BaseFragment() {
 
     private lateinit var backBtn: TextView
     private lateinit var nextBtn: TextView
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.onboard_recorder_fragment, container, false)
+        return inflater.inflate(R.layout.onboard_collect_data, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         initView(view)
     }
 
     override fun onResume() {
         super.onResume()
         (baseActivity as OnBoardActivityInterface).enableSwipe(
-            isSwipeable = true,
-            isTabLayoutVisible = true
+            isSwipeable = true, isTabLayoutVisible = true
         )
         (baseActivity as OnBoardActivityInterface).showButtons(
-            isNextButtonVisible = true,
-            isBackButtonVisible = true
+            isNextButtonVisible = true, isBackButtonVisible = true
         )
-
-
     }
 
     override fun initView(view: View) {
         backBtn = view.findViewById(R.id.back_btn)
+        backBtn.setOnClickListener {
+            baseActivity.onBackPressed()
+        }
         nextBtn = view.findViewById(R.id.next_btn)
         nextBtn.setOnClickListener {
             (baseActivity as OnBoardingActivity).onNextPressed()
