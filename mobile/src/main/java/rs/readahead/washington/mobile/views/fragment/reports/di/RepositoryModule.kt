96ba46d634b285b3b17a56ff60556acbeb9947fe
wafa <lakwafa@gmail.com>
2025-01-14 12:10:23 +0100

    fix conflicts after merge

--- mobile/src/main/java/rs/readahead/washington/mobile/views/fragment/reports/di/RepositoryModule.kt
+++ mobile/src/main/java/rs/readahead/washington/mobile/views/fragment/reports/di/RepositoryModule.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import rs.readahead.washington.mobile.MyApplication
 import rs.readahead.washington.mobile.data.database.DataSource
