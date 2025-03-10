4f48fe9b2195649020b5677ed07b9cc6de675fd6
Ahlem <ahlem.jarrar@hotmail.com>
2025-03-10 12:05:11 +0100

    WIP qr code design

---
+++ mobile/src/main/java/org/horizontal/tella/mobile/views/fragment/peertopeer/QRCodeFragment.kt
@@ -0,0 +1,9 @@
+package org.horizontal.tella.mobile.views.fragment.peertopeer
+
+import org.horizontal.tella.mobile.databinding.FragmentQrCodeBinding
+import org.horizontal.tella.mobile.views.base_ui.BaseBindingFragment
+
+class QRCodeFragment : BaseBindingFragment<FragmentQrCodeBinding>(FragmentQrCodeBinding::inflate) {
+
+    
+}
