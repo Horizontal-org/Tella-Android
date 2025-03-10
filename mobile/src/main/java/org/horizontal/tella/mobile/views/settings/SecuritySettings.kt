1382c32b9470b84ce492ca2a760e81a10afb19b4
wafa lakhal <4533152+wafalakhal@users.noreply.github.com>
2025-03-10 13:16:24 +0100

    fix timeout problem (#369)
    * work on fixing timeout problem
    
    * Fix class name
    
    * fix pr comments

--- mobile/src/main/java/org/horizontal/tella/mobile/views/settings/SecuritySettings.kt
+++ mobile/src/main/java/org/horizontal/tella/mobile/views/settings/SecuritySettings.kt
@@ -3,6 +3,7 @@ package org.horizontal.tella.mobile.views.settings
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.Gravity
 import android.view.View
 import android.widget.CheckBox
