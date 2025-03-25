2ffb9fd84b6e4beb1fbb9ae5956b932fe1043ad2
Ahlem <ahlem.jarrar@hotmail.com>
2025-03-25 11:11:38 +0100

    WIP verification info design

---
+++ mobile/src/main/java/org/horizontal/tella/mobile/views/fragment/peertopeer/activity/PeerToPeerActivity.kt
@@ -0,0 +1,11 @@
+package org.horizontal.tella.mobile.views.fragment.peertopeer.activity
+
+import android.os.Bundle
+import org.horizontal.tella.mobile.views.base_ui.BaseLockActivity
+
+class PeerToPeerActivity  : BaseLockActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
