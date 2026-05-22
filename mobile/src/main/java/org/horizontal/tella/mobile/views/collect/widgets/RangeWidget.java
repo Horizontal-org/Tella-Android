86868dd1524bc6116b3382930db9798dfc9e14bd
Hamsterarsch <git.7uxl1@slmails.com>
2026-05-22 21:45:16 +0200

    feature commit (really)

--- mobile/src/main/java/org/horizontal/tella/mobile/views/collect/widgets/RangeWidget.java
+++ mobile/src/main/java/org/horizontal/tella/mobile/views/collect/widgets/RangeWidget.java
@@ -15,171 +15,102 @@ package org.horizontal.tella.mobile.views.collect.widgets;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
-import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
-import android.widget.CompoundButton;
-import android.widget.CompoundButton.OnCheckedChangeListener;
-import android.widget.ImageButton;
 import android.widget.LinearLayout;
-import android.widget.RadioButton;
-
-import androidx.appcompat.widget.AppCompatRadioButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import org.horizontal.tella.mobile.R;
-import org.horizontal.tella.mobile.util.StringUtils;
-import org.horizontal.tella.mobile.util.Util;
-import org.javarosa.core.model.SelectChoice;
+import org.javarosa.core.model.Constants;
+import org.javarosa.core.model.RangeQuestion;
 import org.javarosa.core.model.data.IAnswerData;
-import org.javarosa.core.model.data.SelectOneData;
-import org.javarosa.core.model.data.helper.Selection;
+import org.javarosa.core.model.data.IntegerData;
 import org.javarosa.form.api.FormEntryPrompt;
-
-import java.util.ArrayList;
-import java.util.List;
 
 
 /**
  * Based on ODK Collect SelectOneWidget.
  */
 @SuppressLint("ViewConstructor")
-public class RangeWidget extends QuestionWidget implements
-        OnCheckedChangeListener {
-    List<SelectChoice> items; // may take a while to compute
-    ArrayList<RadioButton> buttons;
-    ImageButton deleteButton;
+public class RangeWidget extends QuestionWidget
+        implements SeekBar.OnSeekBarChangeListener
+{
+    private SeekBar inputElement;
 
+    private TextView rangeText;
+
+    private int rangeStart;
+    private int range;
 
     public RangeWidget(Context context, FormEntryPrompt prompt) {
         super(context, prompt);
 
-        // deleted: SurveyCTO-added support for dynamic select content (from .csv files)
+        LinearLayout linearLayout = new LinearLayout(context);
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
 
-        items = prompt.getSelectChoices();
-        buttons = new ArrayList<>();
-
-        // Layout holds the vertical list of buttons
-        LinearLayout buttonLayout = new LinearLayout(context);
-        addDeleteButton();
         LayoutInflater inflater = LayoutInflater.from(getContext());
 
-        String s = null;
-        if (prompt.getAnswerValue() != null) {
-            s = ((Selection) prompt.getAnswerValue().getValue()).getValue();
+        inflater.inflate(R.layout.collect_widget_range, linearLayout, true);
+
+
+        RangeQuestion q = (RangeQuestion) prompt.getQuestion();
+
+        rangeStart = q.getRangeStart().intValue();
+        final int rangeEnd = q.getRangeEnd().intValue();
+        range = Math.abs(rangeEnd-rangeStart);
+
+        final int rangeType = prompt.getDataType();
+        if(rangeType == Constants.DATATYPE_INTEGER){
+            inputElement = linearLayout.findViewById(R.id.seekBarDiscrete);
+        }//todo: handle non integer types?
+
+        inputElement.setId(QuestionWidget.newUniqueId());
+        inputElement.setMax(range);
+
+        rangeText = linearLayout.findViewById(R.id.rangeText);
+        rangeText.setId(QuestionWidget.newUniqueId());
+
+        if (formEntryPrompt.getAnswerValue() == null) {
+            setAnswer(range/2);
+        } else {
+            rangeText.setText(formEntryPrompt.getAnswerText());
+            Integer value = ((Integer)formEntryPrompt.getAnswerValue().getValue());
+            inputElement.setProgress(Math.abs(q.getRangeStart().intValue())+value);
         }
 
-        if (items != null) {
-            for (int i = 0; i < items.size(); i++) {
-                String choiceName = prompt.getSelectChoiceText(items.get(i));
-                CharSequence choiceDisplayName;
-                if (choiceName != null) {
-                    choiceDisplayName = StringUtils.markdownToSpanned(choiceName);
-                } else {
-                    choiceDisplayName = "";
-                }
+        inputElement.setOnSeekBarChangeListener(this);
 
-                AppCompatRadioButton r = (AppCompatRadioButton) inflater.inflate(R.layout.collect_radiobutton_item, null);
+        addAnswerView(linearLayout);
+    }
 
-                r.setText(choiceDisplayName);
-                r.setMovementMethod(LinkMovementMethod.getInstance());
-                r.setTag(i);
-                r.setId(QuestionWidget.newUniqueId());
-                r.setEnabled(!prompt.isReadOnly());
-                r.setFocusable(!prompt.isReadOnly());
-                r.setTextColor(getResources().getColor(R.color.wa_white));
-
-                buttons.add(r);
-
-                if (items.get(i).getValue().equals(s)) {
-                    r.setChecked(true);
-                    deleteButton.setVisibility(VISIBLE);
-                }
-
-                r.setOnCheckedChangeListener(this);
-
-                buttonLayout.addView(r);
-            }
-        }
-
-        buttonLayout.setOrientation(LinearLayout.VERTICAL);
-
-        // The buttons take up the right half of the screen
-        addAnswerView(buttonLayout);
+    protected void setAnswer(int value) {
+        inputElement.setProgress(value);
+        rangeText.setText(String.valueOf(value));
     }
 
     @Override
     public void clearAnswer() {
-        for (RadioButton button : this.buttons) {
-            if (button.isChecked()) {
-                button.setChecked(false);
-                clearNextLevelsOfCascadingSelect();
-                deleteButton.setVisibility(GONE);
-                break;
-            }
-        }
+        setAnswer(range/2);
     }
 
     @Override
     public IAnswerData getAnswer() {
-        int i = getCheckedId();
-        if (i == -1) {
-            return null;
-        } else {
-            SelectChoice sc = items.get(i);
-            return new SelectOneData(new Selection(sc));
-        }
+        return new IntegerData(inputElement.getProgress() + rangeStart);
     }
 
     @Override
     public void setFocus(Context context) {
-        // Hide the soft keyboard if it's showing.
-        Util.hideKeyboard(context, this);
-    }
 
-    public int getCheckedId() {
-        for (int i = 0; i < buttons.size(); ++i) {
-            RadioButton button = buttons.get(i);
-            if (button.isChecked()) {
-                return i;
-            }
-        }
-        return -1;
     }
 
     @Override
-    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-        deleteButton.setVisibility(VISIBLE);
-        if (!isChecked) {
-            // If it got unchecked, we don't care.
-            return;
-        }
-
-        for (RadioButton button : buttons) {
-            if (button.isChecked() && !(buttonView == button)) {
-                button.setChecked(false);
-                clearNextLevelsOfCascadingSelect();
-            }
-        }
+    public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+        rangeText.setText(String.valueOf(seekBar.getProgress() - rangeStart));
     }
 
     @Override
-    public void setOnLongClickListener(OnLongClickListener l) {
-        for (RadioButton r : buttons) {
-            r.setOnLongClickListener(l);
-        }
-    }
+    public void onStartTrackingTouch(SeekBar seekBar) {}
 
     @Override
-    public void cancelLongPress() {
-        super.cancelLongPress();
-        for (RadioButton button : this.buttons) {
-            button.cancelLongPress();
-        }
-    }
-
-    private void addDeleteButton() {
-        deleteButton = addButton(R.drawable.ic_cancel_rounded);
-        deleteButton.setContentDescription(getContext().getString(R.string.action_cancel));
-        deleteButton.setOnClickListener(v -> clearAnswer());
-        deleteButton.setVisibility(GONE);
-    }
+    public void onStopTrackingTouch(SeekBar seekBar) {}
 }
