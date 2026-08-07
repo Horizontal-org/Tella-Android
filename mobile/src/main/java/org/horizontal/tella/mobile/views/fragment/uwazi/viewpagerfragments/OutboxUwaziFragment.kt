81a6c7943e4a51e4e298466a16956fe4e644fc62
ahlem <ahlem.jarrar@hotmail.com>
2026-08-07 21:45:25 +0100

    fix T-And-1958 - Update outbox deletion copy

--- mobile/src/main/java/org/horizontal/tella/mobile/views/fragment/uwazi/viewpagerfragments/OutboxUwaziFragment.kt
+++ mobile/src/main/java/org/horizontal/tella/mobile/views/fragment/uwazi/viewpagerfragments/OutboxUwaziFragment.kt
@@ -53,7 +53,8 @@ class OutboxUwaziFragment : BaseReportsFragment<UwaziEntityInstance>() {
                     viewText = getString(R.string.Uwazi_Action_ViewEntity),
                     deleteText = getString(R.string.Uwazi_Action_DeleteEntity),
                     deleteConfirmation = getString(R.string.Uwazi_Subtitle_RemoveOutboxEntity),
-                    deleteActionText = getString(R.string.action_delete) + " \"" + instance.title + "\"?",
+                    deleteActionText = getString(R.string.action_delete)
+                        .replaceFirstChar { it.uppercaseChar() } + " \"" + instance.title + "\"?",
                     confirmButtonLabel = getString(R.string.action_yes),
                     cancelButtonLabel = getString(R.string.action_no),
                 )
